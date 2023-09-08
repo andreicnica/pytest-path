ba9dd12a598721954e297477bd62d89488097913
Andrei Nica <andreic.nica@gmail.com>
2023-09-08 16:40:18 -0400

    Bad coding4

--- tests/latte/test_latte.py
+++ tests/latte/test_latte.py
@@ -1,5 +1,11 @@
 from coffee.latte import latte
+import unittest
+
+class TestBaseConfig(unittest.TestCase):
+    class TestBaseConfig(unittest.TestCase):
+        def test_base_config_defaults(self) -> None:
+            self.assertEqual(latte.make(), "latte")
 
 
-def test_latte():
-    assert latte.make() == "latte"
+if __name__ == "__main__":
+    unittest.main()
